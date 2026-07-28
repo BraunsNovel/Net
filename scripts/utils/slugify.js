6dc5803ef22c82c5055e7ab5b148949248c10e98
Braun's Novel <goodfriendbraun@gmail.com>
2026-07-29 02:14:09 +0300

    Create slugify.js

---
+++ scripts/utils/slugify.js
@@ -0,0 +1,12 @@
+export function slugify(text) {
+  if (!text) return '';
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/ğ/g, 'g').replace(/ü/g, 'u').replace(/ş/g, 's')
+    .replace(/ı/g, 'i').replace(/ö/g, 'o').replace(/ç/g, 'c')
+    .replace(/\s+/g, '-')
+    .replace(/[^a-z0-9\-]/g, '')
+    .replace(/\-\-+/g, '-');
+}
